e49434d80dfb49ef5a46b4b62812b097df9bedd7
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:33:15 +0100

    Update A - Magnets.cpp

--- A - Magnets/A - Magnets.cpp
+++ A - Magnets/A - Magnets.cpp
@@ -1,1 +1,27 @@
-
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    using namespace std;
+    int main()
+    {
+        int n ;
+        vector<int> magnets;
+        cin >> n ;
+        while (n--){
+            int x;
+            cin >> x ;
+            magnets.push_back(x);
+        }
+        int nb = 1 ;
+        for (int i=0 ; i<magnets.size()-1; i++)
+        {
+            if (magnets[i]!=magnets[i+1]){
+     
+                nb++;
+     
+            }
+     
+     
+        }
+        cout << nb ;
+     
+    }
