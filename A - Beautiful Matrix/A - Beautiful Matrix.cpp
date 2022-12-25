b2b77d8897f2d8cc2d02600bf398eea232e4035b
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:30:41 +0100

    Create A - Beautiful Matrix.cpp

---
+++ A - Beautiful Matrix/A - Beautiful Matrix.cpp
@@ -0,0 +1,38 @@
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    using namespace std;
+     
+    int main(){
+    int n,posj,posi;
+    for (int i=1; i<6 ; i ++ ){
+        for (int j = 1 ; j <6 ; j ++ ){
+            cin >> n;
+            if (n==1){
+                posi=i;
+                posj=j;
+     
+            }
+        }
+    }
+    if (posi>3){
+        posi=posi-3;
+    }
+    else if (posi<3){
+        posi=3-posi;
+    }
+    else{
+        posi=0;
+    }
+     
+    if (posj>3){
+        posj=posj-3;
+    }
+    else if (posj<3){
+        posj=3-posj;
+    }
+    else {
+        posj=0;
+    }
+     
+    cout << posi+posj;
+    }
