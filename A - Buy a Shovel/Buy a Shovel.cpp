f2fe76faf3a12a71320a541aba259cd08879b871
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:38:03 +0100

    Create Buy a Shovel.cpp

---
+++ A - Buy a Shovel/Buy a Shovel.cpp
@@ -0,0 +1,28 @@
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #define ll long long int
+    #define db double
+    using namespace std;
+     
+    ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;}
+    ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);}
+     
+    ll isprime(int n){
+        for(int i=2;i*i<=n;i++)if(n%i==0)return 0;
+        return n!=1;}
+     
+     
+     
+     
+     
+     
+    int main(){
+        int k ;
+        int r ;
+        cin >> k >> r;
+        int n = k ;
+        while ((n%10!=r) && (n%10!=0)){
+            n=n+k;
+        }
+        cout << n/k;
+    }
