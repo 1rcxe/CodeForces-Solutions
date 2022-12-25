714c8686aa4ade9e560b225d9fb46f254eee07b6
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:38:27 +0100

    Create A - Bear and Big Brother.cpp

---
+++ A - Bear and Big Brother/A - Bear and Big Brother.cpp
@@ -0,0 +1,23 @@
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #define ll long long int
+    #define db double
+    using namespace std;
+     
+    ll gcd(ll a , ll b) {return b ? gcd(b , a % b) : a ;}
+    ll lcm(ll a , ll b) {return (a * b) / gcd(a , b);}
+     
+    ll isprime(int n){
+        for(int i=2;i*i<=n;i++)if(n%i==0)return 0;
+        return n!=1;}
+     
+    int main(){
+        int a,b,y=0;
+        cin >> a >> b;
+        while (a<=b){
+            a=a*3;
+            b=b*2;
+            y++;
+        }
+        cout << y << endl;
+    }
