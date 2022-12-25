34d9acfc354f951fc2209f4591fb20c2d495cfd8
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:31:43 +0100

    Create Stones on the Table.cpp

---
+++ A - Stones on the Table/Stones on the Table.cpp
@@ -0,0 +1,19 @@
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    using namespace std;
+    int main(){
+        int n,nb=0;
+        string s;
+        cin >> n ;
+        cin >> s ;
+        for (int i=0 ; i < n-1 ;i ++){
+            if (s[i]==s[i+1]){
+                nb++;
+            }
+     
+        }
+        cout << nb ;
+     
+     
+     
+    }
