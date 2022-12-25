2f28f64dd04b849397893f625c36b0d8abfdf6a9
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:27:28 +0100

    Create  A - Anton and Danik.cpp

---
+++ A - Anton and Danik/A - Anton and Danik.cpp
@@ -0,0 +1,23 @@
+    #include <bits/stdc++.h>
+    #define PB push_back
+    #define MP make_pair
+    using namespace std;
+    typedef long long ll;
+    typedef vector<int> vi;
+     
+     
+     
+    int main(){
+        int dan=0,ant=0,n;
+        string Results;
+        cin >> n ;
+        cin >> Results;
+        while (n--){
+            if (Results[n]=='A'){ant++;}
+            else{dan++;}}
+        if (ant>dan){cout << "Anton";}
+        else if (dan>ant){cout << "Danik";}
+        else{ cout << "Friendship";}
+     
+     
+    }
