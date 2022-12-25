834aeed31d8b2c615f2df5479752195ad4d79f4a
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:35:06 +0100

    Create  A - Black Square.cpp

---
+++ A - Black Square/A - Black Square.cpp
@@ -0,0 +1,26 @@
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    using namespace std;
+    int main (){
+        vector <int> calories;
+        string s ;
+        int n = 4 ,nb=0;
+        while(n--){
+            int x ;
+            cin >> x;
+            calories.push_back(x);
+        }
+        cin >> s ;
+        for(int i = 0 ; i < s.size() ; i ++){
+            nb+=calories[int(s[i])-49];
+     
+        }
+     
+        cout << nb;
+     
+     
+     
+     
+     
+    }
+
