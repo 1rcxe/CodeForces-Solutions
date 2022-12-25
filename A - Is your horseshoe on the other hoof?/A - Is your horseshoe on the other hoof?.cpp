72322e444fe0bc555939dc45bc479b3213a75721
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-25 18:29:03 +0100

    Create  	A - Is your horseshoe on the other hoof?.cpp

---
+++ A - Is your horseshoe on the other hoof?/A - Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+vector <int> vi ;
+int n=4;
+while (n--){
+    long long x;
+    cin >> x ;
+    vi.push_back(x);
+}
+int minimum=0;
+sort(vi.begin(), vi.end());
+for (int i = 0 ; i < 3 ; i++ ){
+    int j=i+1;
+    if (vi[i]!=-1){
+        while (vi[i]==vi[j]){
+            minimum++;
+            vi[j]=-1;
+            j++;
+    }
+    }
+}
+cout << minimum;
+ 
+}
