fb4aab03247bebc09c96db0f521f49d836dac2c5
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-12-27 14:03:58 +0100

    Create A - Number Replacement.cpp

---
+++ A - Number Replacement/A - Number Replacement.cpp
@@ -0,0 +1,45 @@
+    #include <bits/stdc++.h>
+    #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    using namespace std;
+     
+     
+    int main(){
+        int t ;
+        cin >> t ;
+        while(t--){
+            bool flag = true;
+            int n ;
+            cin >> n ;
+            map<int,char> mp;
+            vector<int> v ;
+            for (int i = 0 ; i<n ; i++){
+                int x ;
+                cin >> x ;
+                v.push_back(x);
+                mp[v[i]]=' ';
+            }
+            string s ;
+            cin >> s ;
+            for (int i=0 ; i < n ; i++){
+                if (mp[v[i]]==' '){
+                    mp[v[i]]=s[i];
+                }
+                else{
+                    if (mp[v[i]]!=s[i]){
+                        flag=false;
+                        break;
+                    }
+                }
+     
+            }
+            if (flag){
+            cout << "YES" << '\n';
+            }
+            else{
+            cout << "NO" << '\n';}
+     
+        }
+     
+     
+     
+    }
